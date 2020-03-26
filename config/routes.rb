19a690ec39e2ce67b940061a888b3c12071b9758
Dave Winniford <dwinniford@gmail.com>
2020-03-26 16:36:33 -0500

    tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'users/new'
+
+  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
